4ae2da4f4922d4b96c4c9d8f7339f566f41f33b1
ccasasTayronas <cristian.tayronas@gmail.com>
2020-12-19 11:33:56 -0500

    006

--- sales_la_fe/models/__init__.py
+++ sales_la_fe/models/__init__.py
@@ -1,5 +1,6 @@
 from . import account_move
 from . import partner_economic_sector
 from . import res_partner
+from . import ciiu_code
 #from . import sale_order
 #from . import sales_order_chanels
--- sales_la_fe/models/res_partner.py
+++ sales_la_fe/models/res_partner.py
@@ -11,3 +11,6 @@ class ResPartner(models.Model):
     economic_activity_id = fields.Many2one('economic.activity', 'Actividad económica')
     gender = fields.Selection([('Masculino', 'Masculino'), ('Femenino', 'Femenino'), ('Indeterminado', 'Indeterminado')], 'Genero')
     study_credit = fields.Selection([('Si', 'Si'), ('No', 'No')], 'Estudio de credito aprobado?')
+    ciiu_ids = fields.Many2many('ciiu.code', 'partner_ciiu_rel','partner_id', 'ciiu_id', string='Código ciiu' )
+    
+    
