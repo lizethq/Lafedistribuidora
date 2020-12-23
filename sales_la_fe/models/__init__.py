a0533c94a70b7a0556c1e706ad0f282142176578
ccasasTayronas <cristian.tayronas@gmail.com>
2020-12-23 14:34:33 -0500

    003

--- sales_la_fe/models/__init__.py
+++ sales_la_fe/models/__init__.py
@@ -6,6 +6,7 @@ from . import product_template
 from . import sale_order
 from . import payment_methods_la_fe
 from . import channel_fe
+from . import economic_activity
 
 #from . import sale_order
 #from . import sales_order_chanels
