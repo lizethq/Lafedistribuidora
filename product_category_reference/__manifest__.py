9f4f0b8a3f7ed634ce5499b082f16607fe024846
Jhair Escobar <je@todoo.co>
2020-12-30 21:30:32 -0500

    [ADD] product_category_reference: referencias de productos desde una secuencia en la categoría

---
+++ product_category_reference/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Category Reference",
+
+    'summary': """
+        Product Category Reference
+        """,
+
+    'description': """
+        Product Category Reference
+    """,
+
+    'author': "Todoo SAS",
+    'contributors': ['Pater fg@todoo.co'],
+    'website': "http://www.todoo.co",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Tools',
+    'version': '13.1.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['product','sale_timesheet'],
+
+    # always loaded
+    'data': [
+        'views/product_view.xml',
+    ],
+}
