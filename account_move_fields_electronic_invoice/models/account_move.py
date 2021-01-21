abb9aff9c9b4ad89a81d896e364e90300a6dec96
Cristian Casas <cr@todoo.co>
2021-01-21 09:37:30 -0500

    Changes over account_move

--- account_move_fields_electronic_invoice/models/account_move.py
+++ account_move_fields_electronic_invoice/models/account_move.py
@@ -2,14 +2,32 @@
 import logging
 from odoo import models, fields, api
 from odoo.exceptions import AccessDenied
-
+from collections import defaultdict
+from odoo.exceptions import ValidationError
+from odoo import fields, models
+from odoo.tools import float_is_zero
 logger = logging.getLogger(__name__)
-
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
     
     method_la_fe_id = fields.Many2one('payment.methods.la.fe', string='Forma de pago',compute='_compute_method_la_fe_id' )
+
+    def _get_invoiced_lot_values(self):
+        res = super(AccountMove, self)._get_invoiced_lot_values()
+        for i in res:
+            obje_lot = self.env['stock.production.lot'].search([('name','=', i['lot_name'])])
+            logger.error('Hello everyone test 20')
+            logger.error(res)
+            if len(obje_lot)>1:
+                raise ValidationError('Error')
+            i['removal_date'] = obje_lot.removal_date
+        #lot_values = i
+        logger.error('Hello everyone test 20')
+        logger.error(res)
+        return res
+
+    
     
     @api.onchange('invoice_origin')
     def _compute_method_la_fe_id(self):
@@ -31,7 +49,7 @@ class AccountMove(models.Model):
                     logger.error(ab)
                     logger.error(str_one)
                 else:
-                    str_one = record.invoice_origin
+                    str_one = record.invoice_origin   
                 logger.error('************Errotwo***********')
 
                 logger.error(str_one)
@@ -40,8 +58,7 @@ class AccountMove(models.Model):
                 logger.error(sale_obj)
                 record.method_la_fe_id = sale_obj.method_la_fe_id.id
             else:
-                method_la_fe_id = False
-
+                record.method_la_fe_id = False
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
@@ -50,6 +67,26 @@ class AccountMoveLine(models.Model):
     x_cum = fields.Char('Código CUM', compute='_calculate_fields_product')
     x_invima = fields.Char('Código Invima', compute='_calculate_fields_product')
     x_atc = fields.Char('Código ATC', compute='_calculate_fields_product')
+    removal_date = fields.Date('Fecha de vencimiento')
+
+    def _sale_can_be_reinvoice(self):
+        self.ensure_one()
+        return not self.is_anglo_saxon_line and super(AccountMoveLine, self)._sale_can_be_reinvoice()
+
+
+    def _stock_account_get_anglo_saxon_price_unit(self):
+        self.ensure_one()
+        price_unit = super(AccountMoveLine, self)._stock_account_get_anglo_saxon_price_unit()
+
+        so_line = self.sale_line_ids and self.sale_line_ids[-1] or False
+        if so_line:
+            qty_to_invoice = self.product_uom_id._compute_quantity(self.quantity, self.product_id.uom_id)
+            qty_invoiced = sum([x.product_uom_id._compute_quantity(x.quantity, x.product_id.uom_id) for x in so_line.invoice_lines if x.move_id.state == 'posted'])
+            average_price_unit = self.product_id._compute_average_price(qty_invoiced, qty_to_invoice, so_line.move_ids)
+
+            price_unit = average_price_unit or price_unit
+            price_unit = self.product_id.uom_id._compute_price(price_unit, self.product_uom_id)
+        return price_unit
 
     @api.onchange('product_id')
     def _calculate_fields_product(self):
@@ -69,3 +106,8 @@ class AccountMoveLine(models.Model):
                 record.x_cum = False
                 record.x_invima = False
                 record.x_atc = False
+                
+    
+                
+            
+                
