b88c4ce4f6fe567fe42622f34850818aa1e8435d
nf-todoo <62726399+nf-todoo@users.noreply.github.com>
2021-03-08 21:05:53 -0500

    Update product.py

--- product_category_reference/models/product.py
+++ product_category_reference/models/product.py
@@ -6,6 +6,21 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+class StockQuantInherit(models.Model):
+    _inherit = 'stock.quant'
+
+    comercial_value = fields.Float('Valor comercial', compute='_compute_comercial_value')
+    
+    @api.depends('product_id')
+    def _compute_comercial_value(self):
+        for record in self:
+            if record.product_id.standard_price > 0:
+                record.comercial_value = record.product_id.standard_price
+            else:
+                record.comercial_value = 0
+                
+        
+
 class ProductCategory(models.Model):
     _inherit = 'product.category'
 
