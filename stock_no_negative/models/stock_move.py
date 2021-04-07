653cc8e127ad3e6c2c8c33ed382da862da8992ba
nf-todoo <62726399+nf-todoo@users.noreply.github.com>
2021-04-07 16:21:43 -0500

    Update stock_move.py

--- stock_no_negative/models/stock_move.py
+++ stock_no_negative/models/stock_move.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class StockMove(models.Model):
     _inherit = "stock.move"
     
-    invima_code = fields.Char('Código Invima', related="product_id.invima_code")
+    invima_code = fields.Char('Código Invima')
