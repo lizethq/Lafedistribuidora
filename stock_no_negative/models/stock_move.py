636b533cd5253913db196a68b748ec550b471921
nf-todoo <62726399+nf-todoo@users.noreply.github.com>
2021-04-08 10:07:57 -0500

    Update stock_move.py

--- stock_no_negative/models/stock_move.py
+++ stock_no_negative/models/stock_move.py
@@ -1,7 +1,16 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
     
-    invima_code = fields.Char('Código Invima', related="product_id.x_invima")
+    invima_code = fields.Char('Código Invima', compute="_compute_invima_code")
+    
+    @api.depends('product_id')
+    def _compute_invima_code(self):
+        for record in self:
+            if record.product_id.x_invima:
+                record.invima_code = record.product_id.x_invima
+            else:
+                record.invima_code = False
+                
