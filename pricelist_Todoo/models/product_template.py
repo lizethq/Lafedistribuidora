10e0f709390b48d1b0ad8e7a0886ea3c1a4a43c0
pater-todoo <lp@todoo.co>
2021-04-21 13:35:11 -0500

    Update

--- pricelist_Todoo/models/product_template.py
+++ pricelist_Todoo/models/product_template.py
@@ -9,6 +9,6 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    item_ids = fields.One2many('product.template.pricelist', 'product_id', 'Lista de precios')
+    item_ids = fields.One2many('product.pricelist.item', 'product_tmpl_id', 'Pricelist Items')
 
     
